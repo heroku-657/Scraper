2569b2c6a37f70471fd25d38d2c8403d92b02aaf
heroku-657 <trx67t@gmail.com>
2024-08-18 21:36:43 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "29668449"  # Replace this API ID with your actual API ID
+API_HASH = "584fb1df452c0b0cc977abdaa51d9fab"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "BAHEtGEAYf_jUXlo7S3L0m885Zw_LPkY2hwch_TYlNlWvRXKUAxs0jOno9hKviOMcnGyHoyAPoLcru8QkbKFvIY_QwgAIOpnpJ8wBEhNVBQ_QyWO-Rjyi6Mhufes7ePOP-5jRf7zdjladgcee7GgfZ4Qc6gmjhBA-ga7pjgCqOA06bE7RiJiKFTUa7hXHfnYYB1X30C2gBgGs_HjUJbXbDIllV1VU0UbN-tR3u2rU4erYi8ZJOZgwSS5CZmUYC9A6E-qWxDzQFkHkoBiYglw3nWVpJApVI8Ohi5paj4wji2R6zaIzZwVwRXWjfsFS5dWof41RFlEIRyfJa4sgPtyibJq6GEPjQAAAAGkiFrIAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7540595218:AAFfUKr2YmjmifEBIBWH9H9VL_XZzgl8vTs"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [7055366856, 12345678]
+
+# Limits
+DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
+ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
